5c48ee3df58833db3b3e5659f36ae434188b6cfa
Daniel1Maymon <daniel1maymon@gmail.com>
2023-01-31 15:03:35 +0200

    Add files via upload

---
+++ Flask_blog/flaskblog.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__) # __name__ is the module's name
+
+
+@app.route("/") # the root page
+@app.route("/home")
+def home():
+    return "<h1>Home page</h1>"
+
+@app.route("/about") 
+def about():
+    return "<h1>About Page</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
