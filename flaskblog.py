e27a4e209f47947f634110070835b63b2577f79b
daniel1maymon@gmail.com <daniel1maymon@gmail.com>
2023-01-31 16:47:20 +0200

    add templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) # __name__ is the module's name
 
@@ -6,11 +6,11 @@ app = Flask(__name__) # __name__ is the module's name
 @app.route("/") # the root page
 @app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('home.html')
 
 @app.route("/about") 
 def about():
-    return "<h1>About Page2</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
